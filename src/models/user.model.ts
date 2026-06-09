3d9be69bb1fb9bb7ce7087abfd42f14f1261f535
evan-sachot <evan.sachot@laplateforme.io>
2026-06-09 10:28:41 +0200

    inscription model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,4 +6,20 @@ const findSpotifyId = async (spotifyId: string) => {
   const user = rows as any;
   return user.length > 0 ? user[0] : null;
 };
-export default { findSpotifyId };
+const createUser = async (userData: {
+  spotifyId: string;
+  email: string;
+  username: string;
+}) => {
+  const query =
+    "INSERT INTO users (spotify_id, email, username) VALUES (?,?,?)";
+  const [result] = await pool.execute(query, [
+    userData.spotifyId,
+    userData.email,
+    userData.username,
+  ]);
+  const insertId = (result as any).insertId;
+  return { id: insertId, username: userData.username, email: userData.email };
+};
+
+export default { findSpotifyId, createUser };
