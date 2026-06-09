7201582d16946dc279df0827be247d687f47ae72
evan-sachot <evan.sachot@laplateforme.io>
2026-06-09 11:48:54 +0200

    model envoye token crypter

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import pool from "../config/database.js";
+import { encrypt } from "../utils/crypto.util.js";
 
 const findSpotifyId = async (spotifyId: string) => {
   const query = "SELECT* FROM users WHERE spotify_id = ?";
@@ -10,26 +11,35 @@ const createUser = async (userData: {
   spotifyId: string;
   email: string;
   username: string;
+  access_token: string;
+  refresh_token: string;
+  expire_at: Date;
 }) => {
+  const encryptedRefreshToken = encrypt(userData.refresh_token);
   const query =
-    "INSERT INTO users (spotify_id, email, username) VALUES (?,?,?)";
+    "INSERT INTO users (spotify_id, email, username, access_token, refresh_token, expire_at) VALUES (?,?,?,?,?,?)";
   const [result] = await pool.execute(query, [
     userData.spotifyId,
     userData.email,
     userData.username,
+    userData.access_token,
+    encryptedRefreshToken,
+    userData.expire_at,
   ]);
   const insertId = (result as any).insertId;
   return { id: insertId, username: userData.username, email: userData.email };
 };
 const updateToken = async (
   userId: number,
-  tokens: { access_token: string; refresh_token: string },
+  tokens: { access_token: string; refresh_token: string; expire_at: Date },
 ) => {
+  const encryptedRefreshToken = encrypt(tokens.refresh_token);
   const query =
-    "UPDATE users SET access_token = ?, refresh_token = ? WHERE id = ?";
+    "UPDATE users SET access_token = ?, refresh_token = ?, expire_at = ? WHERE id = ?";
   await pool.execute(query, [
     tokens.access_token,
-    tokens.refresh_token,
+    encryptedRefreshToken,
+    tokens.expire_at,
     userId,
   ]);
 };
