9838ded05f87cbf6d8ec08bfb09a0a622df63358
evan-sachot <evan.sachot@laplateforme.io>
2026-06-09 10:37:44 +0200

    inscription

--- src/controllers/spotify.controller.ts
+++ src/controllers/spotify.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import AppError from "../errors/appError.js";
 import spotifyService from "../services/spotify.service.js";
+import userModel from "../models/user.model.js";
 
 const LoginWithSpotify = (
   req: Request,
@@ -26,15 +27,38 @@ const callback = async (
     }
     const token = await spotifyService.getTokens(code);
     const profile = await spotifyService.getSpotifyProfile(token.access_token);
-    res.json({
-      message: "Connexion réussie",
-      tokens: token,
-      spotifyId: profile.spotifyId,
-      email: profile.email,
-    });
+    const existingUser = await userModel.findSpotifyId(profile.spotifyId);
+    if (existingUser) {
+      res.json({ message: "Connexion réussie", user: existingUser });
+    } else {
+      res.json({
+        message: "Nouvel utilisateur, redirection vers l'inscription",
+        spotifyId: profile.spotifyId,
+        email: profile.email,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { spotifyId, email, username } = req.body;
+    if (!username || username.trim() === "") {
+      throw new AppError("Pseudo obligatoire", 400);
+    }
+    if (!email || email.trim() === "") {
+      throw new AppError("Email obligatoire", 400);
+    }
+    const newUser = await userModel.createUser({ spotifyId, email, username });
+    res.status(201).json({ message: "User created", user: newUser });
   } catch (error) {
     next(error);
   }
 };
 
-export default { LoginWithSpotify, callback };
+export default { LoginWithSpotify, callback, register };
--- src/routes/spotify.routes.ts
+++ src/routes/spotify.routes.ts
@@ -4,6 +4,7 @@ import spotifyController from "../controllers/spotify.controller.js";
 const router = Router();
 
 router.get("/spotify/login", spotifyController.LoginWithSpotify);
+router.post("/spotify/register", spotifyController.register);
 router.get("/spotify/callback", spotifyController.callback);
 
 export default router;
