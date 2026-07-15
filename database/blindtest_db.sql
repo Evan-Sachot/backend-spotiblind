7322a4bd33363f24b735c356b88b0e30fda9ec12
evanwsh <evan.sachot@laplateforme.io>
2026-07-15 16:42:42 +0200

    schema sécuriser

--- database/blindtest_db.sql
+++ database/blindtest_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 06 juil. 2026 à 09:22
+-- Généré le : mer. 15 juil. 2026 à 14:38
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
